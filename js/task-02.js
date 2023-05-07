04833e4e4d2fb9538b348279424e196fcd588609
lilyzeta <128751126+tfullest@users.noreply.github.com>
2023-05-07 11:36:47 -0400

    push

---
+++ js/task-02.js
@@ -0,0 +1,14 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+for (const ingredient of ingredients) {
+  let purps = document.createElement("li");
+  purps.setAttribute(`${ingredients.children}`, ingredients.childrens);
+  purps.classList.add("items");
+  console.log(purps);
+}
