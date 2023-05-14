7b7dbadfaea48704d9d44107a102a269e8cf3459
lilyzeta <128751126+tfullest@users.noreply.github.com>
2023-05-14 13:07:11 -0400

    why

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,6 @@
 const slider = document.getElementById("font-size-control");
 const output = document.getElementById("text");
 
-slider.addEventListener(input, handleEvent);
-function handleEvent() {
-  output.style.fontSize = this.value;
-}
+slider.addEventListener("input", (event) => {
+  output.style.fontSize = `${slider.value}` + "px";
+});
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+const alertMessage = "All fields must be filled in!";
 
+const users = {
+  email: email,
+  password: password,
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value || password.value == null) {
+    alert("All Fields Must Be Filled In!");
+  } else {
+    (users.email = email.value), (users.password = password.value);
+  }
+  console.log(email.value, password.value);
+});
