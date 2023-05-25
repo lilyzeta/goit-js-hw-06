ba7da2c2991ac667b5bd1b0aa54a3803e921b7fb
lilyzeta <128751126+tfullest@users.noreply.github.com>
2023-05-24 20:21:18 -0400

    hopefully ok

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,23 @@
 const form = document.querySelector(".login-form");
-const alertMessage = "All fields must be filled in!";
 
 const users = {
-  email: email,
-  password: password,
+  email: "",
+  password: "",
 };
 
 form.addEventListener("submit", submitAct);
 
 function submitAct(event) {
   event.preventDefault();
+  const email = form.elements.email;
+  const password = form.elements.password;
 
-  const {
-    elements: { email, password },
-  } = event.currentTarget;
-
-  if (email.value === "" || password.value.trim() === "") {
-    return alert("All Fields must be filled in");
+  if (email.value.length === 0 || password.value.length === 0) {
+    alert("All Fields must be filled in");
+  } else {
+    users.email = email.value;
+    users.password = password.value;
+    form.reset();
   }
-  console.log(`Login: ${email.value}, Password: ${password.value.trim()}`);
-  event.currentTarget.reset();
+  console.log(users);
 }
