09eafed263f44a91c8ed0311381df98445a0fdc1
lilyzeta <128751126+tfullest@users.noreply.github.com>
2023-05-28 10:41:44 -0400

    corrected for tutor feedback

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const slider = document.getElementById("font-size-control");
 const output = document.getElementById("text");
-
+output.style.fontSize = `${slider.value}` + "px";
 slider.addEventListener("input", (event) => {
   output.style.fontSize = `${slider.value}` + "px";
 });
